6bc36268b588352537d404f388af354666c9f452
Sarthak Gupta <sarthakgupta072@gmail.com>
2023-08-01 19:55:27 +0530

    Enhance card component

--- src/popup/WeatherCard/WeatherCard.tsx
+++ src/popup/WeatherCard/WeatherCard.tsx
@@ -5,38 +5,58 @@ import Box from "@mui/material/Box"
 import { OpenWeatherData, fetchOpenWeatherData } from "../../utils/api"
 import { Typography } from "@mui/material"
 
+const WeatherCardContainer: React.FC<{
+  children: React.ReactNode
+}> = ({ children }) => {
+  return (
+    <Box mx={"4px"} my={"16px"}>
+      <Card>
+        <CardContent>{children}</CardContent>
+      </Card>
+    </Box>
+  )
+}
+
+type WeatherCardState = "loading" | "error" | "ready"
+
 const WeatherCard: React.FC<{
   city: string
 }> = ({ city }) => {
   const [weatherData, setWeatherData] = useState<OpenWeatherData | null>(null)
+  const [cardState, setCardState] = useState<WeatherCardState>("loading")
   useEffect(() => {
     fetchOpenWeatherData(city)
       .then((data) => {
         setWeatherData(data)
+        setCardState("ready")
       })
       .catch((err) => {
-        console.log(err)
+        setCardState("error")
       })
   }, [city])
 
-  if (!weatherData) {
-    return <div>Loading...</div>
+  if (cardState == "loading" || cardState == "error") {
+    return (
+      <WeatherCardContainer>
+        <Typography variant='body1'>
+          {cardState == "loading"
+            ? "loading..."
+            : `Error: Could not retrieve data for the city: ${city}`}
+        </Typography>
+      </WeatherCardContainer>
+    )
   }
 
   return (
-    <Box mx={"4px"} my={"16px"}>
-      <Card>
-        <CardContent>
-          <Typography variant='h5'>{weatherData.name}</Typography>
-          <Typography variant='body1'>
-            {Math.round(weatherData.main.temp)}
-          </Typography>
-          <Typography variant='body1'>
-            Feels Liks: {Math.round(weatherData.main.feels_like)}
-          </Typography>
-        </CardContent>
-      </Card>
-    </Box>
+    <WeatherCardContainer>
+      <Typography variant='h5'>{weatherData.name}</Typography>
+      <Typography variant='body1'>
+        {Math.round(weatherData.main.temp)}
+      </Typography>
+      <Typography variant='body1'>
+        Feels Liks: {Math.round(weatherData.main.feels_like)}
+      </Typography>
+    </WeatherCardContainer>
   )
 }
 
--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import "./popup.css"
+import "fontsource-roboto"
 import WeatherCard from "./WeatherCard"
 
 const App: React.FC<{}> = () => {
@@ -8,6 +9,7 @@ const App: React.FC<{}> = () => {
     <div>
       <WeatherCard city='Bangalore' />
       <WeatherCard city='Jammu' />
+      <WeatherCard city='Bangal' />
     </div>
   )
 }
