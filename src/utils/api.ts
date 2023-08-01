1bfa523ecd5daa1093c816c2b0082ea31ce71fc4
Sarthak Gupta <sarthakgupta072@gmail.com>
2023-08-01 18:49:22 +0530

    feat: add Weather Card component

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,15 +2,39 @@ import OPEN_WEATHER_API_KEY from "./.properties"
 
 const API_KEY = OPEN_WEATHER_API_KEY
 
-export async function fetchOpenWeatherData(city: string): Promise<any> {
+export interface OpenWeatherData {
+  name: string
+  main: {
+    feels_like: number
+    humidity: number
+    pressure: number
+    temp: number
+    temp_max: number
+    temp_min: number
+  }
+  weather: {
+    description: string
+    icon: string
+    id: number
+    main: string
+  }[]
+  wind: {
+    deg: number
+    speed: number
+  }
+}
+
+export async function fetchOpenWeatherData(
+  city: string
+): Promise<OpenWeatherData> {
   const res = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
   )
 
   if (!res.ok) {
     throw new Error("City not found")
   }
 
-  const data = await res.json()
+  const data: OpenWeatherData = await res.json()
   return data
 }
