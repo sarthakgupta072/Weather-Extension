f3196434a6f95ff284a48f8c14005639051f7a0e
Sarthak Gupta <sarthakgupta072@gmail.com>
2023-09-30 21:27:41 +0530

    feat: add new city in popup without persistence

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,16 +1,52 @@
-import React from "react"
+import React, { useState } from "react"
 import ReactDOM from "react-dom"
+import { Box, Grid, Paper, InputBase, IconButton } from "@mui/material"
+import { Add as AddIcon } from "@mui/icons-material"
+
 import "./popup.css"
 import "fontsource-roboto"
 import WeatherCard from "./WeatherCard"
 
 const App: React.FC<{}> = () => {
+  const [cities, setCities] = useState<string[]>([
+    "Bangalore",
+    "Jammu",
+    "Bangal",
+  ])
+
+  const [cityInput, setCityInput] = useState<string>("")
+
+  const handleCityButtonClick = () => {
+    if (cityInput === "") return
+
+    setCities([...cities, cityInput])
+    setCityInput("")
+  }
+
   return (
-    <div>
-      <WeatherCard city='Bangalore' />
-      <WeatherCard city='Jammu' />
-      <WeatherCard city='Bangal' />
-    </div>
+    <Box mx='8px' my='16px'>
+      <Grid container>
+        <Grid item>
+          <Paper>
+            <Box px='15px' py='5px'>
+              <InputBase
+                placeholder='Add a city name'
+                value={cityInput}
+                onChange={(event) => setCityInput(event.target.value)}
+              />
+              <IconButton onClick={handleCityButtonClick}>
+                <AddIcon />
+              </IconButton>
+            </Box>
+          </Paper>
+        </Grid>
+      </Grid>
+
+      {cities.map((city, index) => (
+        <WeatherCard key={index} city={city} />
+      ))}
+      <Box height='10px' />
+    </Box>
   )
 }
 
